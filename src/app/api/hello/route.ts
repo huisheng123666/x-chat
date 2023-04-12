bf3756457cc9d95a2a60ba87344270056120d4f5
xmw <marvin@hbtgft.com>
2023-04-12 10:51:36 +0800

    feat: openai

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -17,12 +17,18 @@ export async function POST(request: Request) {
   if (!openApi) {
     openApi = new OpenAIApi(configuration);
   }
-  const body = await request.json()
-  const res = await openApi.createCompletion({
-    model: "text-davinci-003",
-    prompt: body.msg,
-    temperature: 0,
-    max_tokens: 7,
-  })
-  return NextResponse.json(res)
+  try {
+    const body = await request.json()
+    const res = await openApi.createCompletion({
+      model: "text-davinci-003",
+      prompt: body.msg,
+      temperature: 0,
+      max_tokens: 7,
+    })
+    return NextResponse.json(res)
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json({ code: 1 })
+  }
+
 }
