7f31e72727a65a1dd609f2a26c162c5367b5aaac
xmw <marvin@hbtgft.com>
2023-04-12 16:13:13 +0800

    feat: chat api

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,6 +1,1 @@
-self.__NEXT_FONT_MANIFEST={
-  "pages": {},
-  "app": {},
-  "appUsingSizeAdjust": false,
-  "pagesUsingSizeAdjust": false
-}
+self.__NEXT_FONT_MANIFEST={pages:{},app:{'/Users/xmw/my-project/chat-gpt/node_modules/.pnpm/next@13.3.0_biqbaboplfbrettd7655fr4n2y/node_modules/next/font/google/target.css?{"path":"src/app/page.tsx","import":"Inter","arguments":[{"subsets":["latin"]}],"variableName":"inter"}':["static/media/2aaf0723e720e8b9-s.p.woff2"]},appUsingSizeAdjust:!0,pagesUsingSizeAdjust:!1};
